8cb91435e9cc70010c0a652e5837a3c3272c803b
pranavmehta95 <pranavmehta192@gmail.com>
2026-01-27 23:49:02 +0530

    Revise js

---
+++ JS/index.js
@@ -0,0 +1,53 @@
+// console.log("Hello there.")
+
+// const age = 30;
+// let name = "pranav";
+// console.log(name);
+
+
+// let firstname = "pranav";
+// console.log("Hello"+firstname);
+// console.log("namaste"+firstname);
+// console.log("Suno"+firstname);
+
+// let firstname1 = "John";
+// console.log("Hello"+firstname1);
+// console.log("Hello"+firstname1);
+// console.log("Hello"+firstname1);
+
+
+// function greet(nameOfThePerson){
+//     console.log("Hello"+firstname);
+//     console.log("namaste"+firstname);
+//     console.log("Suno"+firstname);
+
+// }
+
+// let firstname = "pranv";
+// let firstname1 = "John";
+// let firstname2 = "Ram";
+
+// greet(firstname);
+// greet(firstname1);
+// greet(firstname2);
+
+function islegel(name, age){
+    if(age >= 18){
+        console.log(name + " is allowed to vote");
+    }else{
+        console.log(name + " is not allowed to vote");
+    }
+}
+var user1 = {
+    name: "Pranav",
+    age: 22,
+    password: "eufyufh3"
+}
+
+var user2 = {
+    name: "John",
+    age: 12,
+    password: ""
+}
+islegel(user1.name, user1.age);
+islegel(user2.name, user2.age);
